648357aa86d6f71785670febafd3c12a147096a6
smart8bits <smart8bits@gmail.com>
2021-02-18 20:50:34 +0330

    initial commit

---
+++ app/src/main/java/com/smart8bits/randomnotegenerator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smart8bits.randomnotegenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
